ff7894b08f7eceb285749b4bdecb6ba86426fe13
Aysuhgarg <Coolayushgarg222@gmail.com>
2020-06-22 18:40:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/whatsapp/Call_Frag.java
@@ -0,0 +1,32 @@
+package com.example.whatsapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class Call_Frag extends Fragment {
+
+    public Call_Frag() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        //View view = inflater.inflate(R.layout.fragment_call_, container, false);
+        return inflater.inflate(R.layout.fragment_call_, container, false);
+
+    }
+
+
+
+
+}
